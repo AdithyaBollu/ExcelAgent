07168322059d0673d7d4171f98f74e1032a44711
Adithya Bollu <adithya.bollu@gmail.com>
2025-08-12 15:51:31 -0700

    Files and Design changes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,13 @@
-from flask import request, jsonify
+from pathlib import Path
+from flask import json, request, jsonify
 from . import app
 from app.agent.agent import chat_with_nutrition_bot
+import openpyxl
+import tempfile
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/')
@@ -20,6 +27,45 @@ def handle_chat():
             'response': bot_response
         })
     except Exception as e:
+        return jsonify({
+            'error': str(e)
+        }), 500
+    
+
+
+@app.route('/chat-with-files', methods=['POST'])
+def handle_chat_with_files():
+    try:
+        user_message = request.form.get('message', '')
+        history = request.form.get('history', [])
+        file_count = int(request.form.get('file_count', 0))
+        history = json.loads(history)
+        
+        temp_file_paths = dict()
+
+        for i in range(file_count):
+            file = request.files.get(f'file_{i}')
+                
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                file.save(temp_file.name)
+                logging.info(f"File saved to temporary location: {temp_file.name}")
+                temp_file_paths[file.filename] = temp_file.name
+        
+
+        enhanced_user_message = f"{user_message} Files: "
+        for file in temp_file_paths:
+            enhanced_user_message += f"{file} at {temp_file_paths[file]}, "
+        logging.info(f"Enhanced user message: {enhanced_user_message}")
+
+
+        bot_response = chat_with_nutrition_bot(enhanced_user_message, history)
+
+        return jsonify({
+            "response": bot_response
+        })
+    except Exception as e:
+        logging.error(f"Error in handle_chat_with_files: {e}")
+        logging.info(e)
         return jsonify({
             'error': str(e)
         }), 500
--- frontend/viteApp/src/components/Chatbox/ChatBox.tsx
+++ frontend/viteApp/src/components/Chatbox/ChatBox.tsx
@@ -18,7 +18,9 @@ interface ChatBoxProps {
 }
 
 const ChatBox : React.FC<ChatBoxProps> = ({messages, setMessages, setMessageSent, setGenerating}) => {
-  const textRef = useRef(null);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const [files, setFiles] = useState<File[]>([]);
   const [message, setMessage] = useState("");
 
   function handleInput(e: React.FormEvent<HTMLTextAreaElement>) {
@@ -41,7 +43,18 @@ const ChatBox : React.FC<ChatBoxProps> = ({messages, setMessages, setMessageSent
     
   }
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const fileArray = Array.from(files);
+      setFiles(fileArray);
+      // Optionally, you can also handle the files here
+      console.log("Files selected:", fileArray);
+    }
+  }
+
   async function handleSend() {
+    console.log("Message:", message);
     if (message.trim() === "") return; // Prevent sending empty messages
 
     try {
@@ -89,6 +102,64 @@ const ChatBox : React.FC<ChatBoxProps> = ({messages, setMessages, setMessageSent
     // For example, you could call an API to get the assistant's reply
   }
 
+  async function handleSendWithFiles() {
+    console.log("Files:", files);
+    // if (message.trim() === "" ) return; // Prevent sending empty messages or without files
+
+    try {
+      console.log("Sending message with files:", message, files);
+      setMessageSent(true);
+      setGenerating(true);
+      const newMessage: Message = {
+        role: "user",
+        content: message
+      };
+
+      if (textRef.current) {
+        const textarea = textRef.current as HTMLTextAreaElement;
+        textarea.value = ""; // Clear the textarea
+        textarea.style.height = getComputedStyle(textarea).minHeight; // Reset height
+      }
+
+      setMessages(prevMessages => [...prevMessages, newMessage]);
+
+      const formData = new FormData();
+
+      formData.append("message", message);
+      formData.append("history", JSON.stringify(messages));
+      console.log("Files length:", files.length);
+      formData.append("file_count", files.length.toString());
+
+      files.forEach((file, index) => {
+        formData.append(`file_${index}`, file);
+      });
+
+      // formData.append("fileCount", files.length.toString());
+
+      let res = await fetch("http://localhost:5000/chat-with-files", {
+        method: "POST",
+        body: formData
+      });
+
+
+      const data = await res.json();
+      const botMessage: Message = {
+        role: "assistant",
+        content: data.response
+      };
+      
+      setMessages(prevMessages => [...prevMessages, botMessage]);
+      setMessage(""); // Clear the input after sending
+      setFiles([]); // Clear files after sending
+      setGenerating(false); // Stop generating state
+
+      // Handle file upload logic here if needed
+
+    } catch (error) {
+      console.error("Error sending message with files:", error);
+    }
+  }
+ 
   useEffect(() => {
     if (textRef.current) {
       const textarea = textRef.current as HTMLTextAreaElement; 
@@ -101,17 +172,35 @@ const ChatBox : React.FC<ChatBoxProps> = ({messages, setMessages, setMessageSent
       <div className="chat-box-container">
         
         <div className="tools-container">
-          <textarea
+          <div className="input-container">
+            <textarea
             className="inp"
             ref={textRef}
             placeholder="Type your message here..."
             onInput={handleInput}
           />
+          <div className="file-list">
+            {files.length > 0 && (
+              <div className="file-list-ul">
+                {files.map((file, index) => (
+                  <div key={index} className="file-list-item">
+                    {file.name}
+                    <button className="remove-file-btn" onClick={() => {
+                      setFiles(prevFiles => prevFiles.filter((_, i) => i !== index));
+                    }}>X</button>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          </div>
+          
           <div className="tools">
-            <button className="sendBtn btn" onClick={handleSend}>
+            <button className="sendBtn btn" onClick={() => files.length === 0 ? handleSend() : handleSendWithFiles()}>
               <img className="send-icon" src={SendIcon} alt="Send" />
             </button>
-            <button className="attachBtn btn">
+            <input type="file" ref={fileRef} onChange={handleFileChange} multiple accept=".pdf,.txt,.csv,.xlsx"style={{ display: 'none' }}/>
+            <button onClick={() => fileRef.current ? fileRef.current.click() : ""} className="attachBtn btn">
               <img className="attach-icon" src={AttachIcon} alt="Send" />
             </button>
           </div>
